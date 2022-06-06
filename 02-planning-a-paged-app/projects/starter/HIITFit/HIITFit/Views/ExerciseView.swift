a73bb7d9dfb7b934f68a93367cedef6ebae4d5ec
NguyenT01 <transinguyen2001@gmail.com>
2022-06-06 20:14:37 +0700

    chapter02 complete

---
+++ 02-planning-a-paged-app/projects/starter/HIITFit/HIITFit/Views/ExerciseView.swift
@@ -0,0 +1,20 @@
+
+
+import SwiftUI
+
+let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
+let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
+
+struct ExerciseView: View {
+    let index: Int
+    
+    var body: some View {
+        Text(exerciseNames[index])
+    }
+}
+
+struct ExerciseView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExerciseView(index: 0)
+    }
+}
